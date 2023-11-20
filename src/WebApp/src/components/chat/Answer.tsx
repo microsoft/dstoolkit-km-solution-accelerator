d1c829574ea7c9f70f1618cc8f3156f2d6bbd7cc
Francesco Sparacio <franspa@microsoft.com>
2023-11-21 00:26:09 +0100

    visual improvements

--- src/WebApp/src/components/chat/Answer.tsx
+++ src/WebApp/src/components/chat/Answer.tsx
@@ -1,6 +1,6 @@
 import { Spinner } from "@fluentui/react-components";
 import { AskResponse, ChatApiResponse } from "../../api/models";
-import { InteractionTag, InteractionTagPrimary } from "@fluentui/react-tags-preview";
+import { InteractionTag, InteractionTagPrimary, TagGroup } from "@fluentui/react-tags-preview";
 
 interface Props {
     answer: ChatApiResponse;
@@ -13,16 +13,36 @@ export function Answer({ answer, loading }: Props) {
             <div className="ml-5 max-w-[900px] overflow-auto break-words rounded-md bg-[#FFFFFF] px-10 py-5 shadow-sm outline outline-1 outline-transparent">
                 {loading && <Spinner />}
                 {answer.answer}
-                {!loading && answer.followUpQs && <p>Follow Up Questions: {answer.followUpQs}</p>}
 
-                <div className="mt-10">
-                    <InteractionTag>
-                        <InteractionTagPrimary>
-                    {!loading && answer.references &&
-                        answer.references.map((reference, index) => <p key={index}>Reference: {reference.chunkId.substring(0, 50)}...</p>)}
+                {/* <div>
+                    {!loading && answer.followUpQs && answer.followUpQs.length > 0 && <div>Follow Up Questions: {answer.followUpQs.map(txt => <p>{txt}</p>)}</div>}
+                </div> */}
+
+            <div className="mt-5">
+                { answer.references.map((reference, index) => (
+                    <InteractionTag size="small">
+                        <InteractionTagPrimary>                    
+                         <p key={index}>[{index + 1}] {reference.chunkId.substring(0, 80)}...</p>
                         </InteractionTagPrimary>
                     </InteractionTag>
-                </div>
+                ))}
+            </div>
+
+            <div className="mt-5">
+                { answer.followUpQs.map((question, index) => (
+                    <InteractionTag size="small">
+                        <p className="mt-2">
+                            <InteractionTagPrimary>                    
+                                <p key={index}>{question}</p>
+                            </InteractionTagPrimary>
+                        </p>
+                    </InteractionTag>
+                ))}
+            </div>
+
+            
+
+               
             </div>
         </div>
     );
